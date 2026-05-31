3f1ac8318432d15142e229e4577bc56d8ae1f409
Wlad Paiva <desenvolvedormg@gmail.com>
2026-05-31 12:11:30 -0300

    change playwright dev settings

--- apps/website/playwright.config.ts
+++ apps/website/playwright.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   use: {
     baseURL: "http://127.0.0.1:4321",
     trace: "on-first-retry",
+    headless: !!process.env.CI,
   },
   projects: [
     {
@@ -20,6 +21,7 @@ export default defineConfig({
   ],
   webServer: {
     command: "vpx varlock run -- astro dev --host 127.0.0.1 --port 4321",
+    stdout: "pipe",
     url: "http://127.0.0.1:4321",
     reuseExistingServer: !process.env.CI,
     env: {
