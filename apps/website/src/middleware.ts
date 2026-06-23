24aac794a577661c1965689f463ae2910b4e0a4d
Wlad Paiva <desenvolvedormg@gmail.com>
2026-06-23 14:24:58 -0300

    remove service name from init logger

--- apps/website/src/middleware.ts
+++ apps/website/src/middleware.ts
@@ -10,10 +10,7 @@ const drain = ENV.POSTHOG_KEY
     })
   : undefined;
 
-initLogger({
-  env: { service: "website" },
-  ...(drain ? { drain } : {}),
-});
+initLogger(drain ? { drain } : {});
 
 export const onRequest = defineMiddleware(async ({ request, locals }, next) => {
   const url = new URL(request.url);
