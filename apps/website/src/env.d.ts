c16cb7ffe41266f52d754112485dc0fc6b06dc93
Wlad Paiva <desenvolvedormg@gmail.com>
2026-07-01 10:16:19 -0300

    Add PostHog types to website

--- apps/website/src/env.d.ts
+++ apps/website/src/env.d.ts
@@ -1,8 +1,13 @@
 /// <reference types="astro/client" />
 
+import type { PostHog } from "@posthog/types";
 import type { RequestLogger } from "evlog";
 
 declare global {
+  interface Window {
+    posthog?: PostHog;
+  }
+
   namespace App {
     interface Locals {
       log: RequestLogger;
